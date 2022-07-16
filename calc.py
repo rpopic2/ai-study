3bd90831b336f33a6db73e75225b6f3eb9936f23
rpopic2 <rpopic2@gmail.com>
2022-07-17 00:37:15 +0900

    added parsing expr

--- calc.py
+++ calc.py
@@ -3,11 +3,18 @@
 #     addend = int(input[2])
 #     return augend + addend
 
-def calculate(input):
+def calculate(input: str):
     augend, expr, addend = input.partition('+')
     return int(augend) + int(addend)
 
+
+def parse_expr(input: str):
+    for x in input:
+        if not x.isnumeric():
+            return x
+
+
 if __name__ == "__main__":
     import sys
-    x= calculate(sys.argv[1])
+    x = calculate(sys.argv[1])
     print(x)
--- calctest.py
+++ calctest.py
@@ -7,3 +7,9 @@ class calctest(unittest.TestCase):
         self.assertEqual(5, calc.calculate("3+2"))
         self.assertEqual(3, calc.calculate("1+2"))
         self.assertEqual(13, calc.calculate("10+3"))
+        self.assertEqual(13, calc.calculate("10 + 3"))
+        
+    def testparseexpr(self):
+        self.assertEqual('+', calc.parse_expr("3+2"))
+        self.assertEqual('-', calc.parse_expr("3-2"))
+        self.assertEqual('-', calc.parse_expr("3-2="))
