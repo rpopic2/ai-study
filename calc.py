85a9ea3f5ea0d8407a6ba078a819c3c74b698d94
rpopic2 <rpopic2@gmail.com>
2022-07-17 00:20:22 +0900

    works with bigger numbers

--- calc.py
+++ calc.py
@@ -1,4 +1,13 @@
-def calcualte(input):
-    augend = int(input[0])
-    addend = int(input[2])
-    return augend + addend
+# def calculate(input):
+#     augend = int(input[0])
+#     addend = int(input[2])
+#     return augend + addend
+
+def calculate(input):
+    augend, expr, addend = input.partition('+')
+    return int(augend) + int(addend)
+
+if __name__ == "__main__":
+    import sys
+    x= calculate(sys.argv[1])
+    print(x)
--- calctest.py
+++ calctest.py
@@ -4,5 +4,6 @@ import calc
 
 class calctest(unittest.TestCase):
     def testaddition(self):
-        self.assertEqual(5, calc.calcualte("3+2"))
-        self.assertEqual(3, calc.calcualte("1+2"))
+        self.assertEqual(5, calc.calculate("3+2"))
+        self.assertEqual(3, calc.calculate("1+2"))
+        self.assertEqual(13, calc.calculate("10+3"))
