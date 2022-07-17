196235f029eba840bb5a4bed904f8f777d03955f
rpopic2 <rpopic2@gmail.com>
2022-07-17 22:56:25 +0900

    using fixture function

--- calctest.py
+++ calctest.py
@@ -1,16 +1,18 @@
 import unittest
 import calc
 
+
 def define_basic_op():
     calc.define_operator('+', lambda x, y: x+y)
     calc.define_operator('-', lambda x, y: x-y)
     calc.define_operator('*', lambda x, y: x*y)
     calc.define_operator('/', lambda x, y: x/y)
-class calctest(unittest.TestCase):
 
+
+class calctest(unittest.TestCase):
+    define_basic_op()
 
     def test_addition(self):
-        calc.define_operator('+', lambda x, y: x+y)
         self.assertEqual(5, calc.main("3+2"))
         self.assertEqual(13, calc.main("10+3"))
         self.assertEqual(13, calc.main("10 + 3"))
@@ -18,19 +20,16 @@ class calctest(unittest.TestCase):
         self.assertEqual(13, calc.main("10 + 3    "))
 
     def test_subtraction(self):
-        calc.define_operator('-', lambda x, y: x-y)
         self.assertEqual(2, calc.main("5 - 3"))
 
     def test_unknown_operator_error(self):
         self.assertEqual(None, calc.main("3}2"))
 
     def test_non_numeric_error(self):
-        calc.define_operator('-', lambda x, y: x-y)
         self.assertEqual(None, calc.main("a-2"))
         self.assertEqual(None, calc.main("2-a"))
 
     def test_new_calc(self):
-        calc.define_operator('+', lambda x, y: x+y)
         self.assertEqual(5, calc.calculate(2, "+", 3))
 
 
